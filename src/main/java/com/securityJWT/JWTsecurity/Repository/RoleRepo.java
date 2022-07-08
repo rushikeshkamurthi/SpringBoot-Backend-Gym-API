fef182142c3048bce673b71c53cebb35f4795351
rushikeshkamurthi <k.hrushikesh007@gmail.com>
2022-07-08 16:56:34 +0530

    modified Role repo to check pull request

--- src/main/java/com/securityJWT/JWTsecurity/Repository/RoleRepo.java
+++ src/main/java/com/securityJWT/JWTsecurity/Repository/RoleRepo.java
@@ -5,6 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface RoleRepo extends JpaRepository<Role,Long> {
 
     Role findByName(String name); // it  returns the Role if we provide name of the role
-
-
 }
